fd4fe3933364eed57b4a2ee9d5e6552214e9512b
ashiqulemu <ashiqulemu.jpi@gmail.com>
2025-06-23 00:21:49 +0600

    efewff

--- wp-content/themes/twentytwentyfour/functions.php
+++ wp-content/themes/twentytwentyfour/functions.php
@@ -204,3 +204,65 @@ if ( ! function_exists( 'twentytwentyfour_pattern_categories' ) ) :
 endif;
 
 add_action( 'init', 'twentytwentyfour_pattern_categories' );
+
+
+// ======================================
+
+
+// Stop Elementor Pro from checking for updates
+add_filter( 'site_transient_update_plugins', 'disable_elementor_pro_update_check' );
+
+function disable_elementor_pro_update_check( $transient ) {
+    // Define the plugin's path
+    $plugin = 'elementor-pro/elementor-pro.php';
+
+    // Check if Elementor Pro is listed in the update transient
+    if ( isset( $transient->response[$plugin] ) ) {
+        unset( $transient->response[$plugin] ); // Remove the update information for Elementor Pro
+    }
+
+    return $transient;
+}
+
+
+// Hide Elementor Pro license activation message
+add_filter( 'elementor_pro/license/valid', '__return_true' );
+
+
+
+
+// Add custom CSS to hide Elementor Pro activation message in the WordPress Dashboard
+function hide_elementor_pro_activation_message() {
+    echo '<style>
+        .e-notice.e-notice--extended {
+            display: none !important;
+        }
+		.notice.notice-warning.is-dismissible {
+		  display: none;
+		}
+    </style>';
+}
+add_action( 'admin_head', 'hide_elementor_pro_activation_message' );
+
+
+// Hide Elementor License_submenu
+function hide_elementor_license_submenu() {
+    remove_submenu_page( 'elementor', 'elementor-license' );
+}
+add_action( 'admin_menu', 'hide_elementor_license_submenu', 999 );
+
+
+
+
+function hide_elementor_connect_text() {
+    echo '<style>
+        .active_license a.elementor-plugins-gopro {
+            font-size: 0 !important;
+        }
+        .active_license a.elementor-plugins-gopro::after {
+            content: " "; /* This prevents layout shifts */
+            font-size: initial;
+        }
+    </style>';
+}
+add_action('admin_head', 'hide_elementor_connect_text');
